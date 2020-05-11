975fadfc2e625b624822569e951de5e887e8e721
Fernando B Oliveira <fboliveira25@gmail.com>
2020-05-11 12:44:28 -0300

    Example - base code

---
+++ 01-example-site/data.js
@@ -0,0 +1,14 @@
+const data = [
+
+    { name: 'Ana', gender: 'F' },
+    { name: 'Artur', gender: 'M'},
+    { name: 'Bernardo', gender: 'M'},
+    { name: 'Brígida', gender: 'F'},
+    { name: 'Hugo', gender: 'M'},
+    { name: 'Larissa', gender: 'F'},
+    { name: 'Marina', gender: 'F'},
+    { name: 'João', gender: 'M'},
+    { name: 'Pedro', gender: 'M'},
+    { name: 'Sofia', gender: 'F'}
+    
+];
